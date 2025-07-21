b4ca8626894c87462debd4389b5287e61655e719
Arjun Sharma <arjun.sharma07@outlook.com>
2025-07-21 00:44:49 -0500

    Remove unnecssary device call

--- src/util/distributions.py
+++ src/util/distributions.py
@@ -1,10 +1,10 @@
 import torch
 import numpy as np
 
-def sample_massless_4momentum_clouds(n_clouds, cloud_size, device):
-    E_c = torch.exp(torch.randn(n_clouds, cloud_size, device=device))  # Sample energy from appropriate distribution
-    theta = np.arccos(1 - 2*torch.rand(n_clouds, cloud_size, device=device))
-    phi = 2*np.pi*torch.rand(n_clouds, cloud_size, device=device)
+def sample_massless_4momentum_clouds(n_clouds, cloud_size):
+    E_c = torch.exp(torch.randn(n_clouds, cloud_size))  # Sample energy from appropriate distribution
+    theta = np.arccos(1 - 2*torch.rand(n_clouds, cloud_size))
+    phi = 2*np.pi*torch.rand(n_clouds, cloud_size)
     p_x = E_c * np.sin(theta) * np.cos(phi)
     p_y = E_c * np.sin(theta) * np.sin(phi)
     p_z = E_c * np.cos(theta)
