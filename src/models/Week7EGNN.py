a6e94214e8f1e2d3a69943b1a3683d994851433a
Arjun Sharma <arjun.sharma07@outlook.com>
2025-08-18 15:20:02 -0500

    Add RK2

--- src/models/Week7EGNN.py
+++ src/models/Week7EGNN.py
@@ -343,14 +343,20 @@ class JetFlowMatcher(nn.Module):
         else:
             return x * mask.unsqueeze(-1)
 
-    def step(self, x_t, jet_conditions, mask, t_start, t_end, method='euler'):
+    def step(self, x_t, jet_conditions, mask, t_start, t_end, method='RK2'):
         """
         Calculate the probability density at a particular time step
         """
+        batch_size = x_t.shape[0]
         if method == 'euler':
-            batch_size = x_t.shape[0]
             update = self.forward(x=x_t, t=t_start.unsqueeze(0).repeat(batch_size), jet_conditions=jet_conditions, mask=mask)
             x_next = x_t + update * (t_end - t_start)
+            return x_next
+        elif method == 'RK2':
+            start_vel = self.forward(x_t=x_t, t=t_start.unsqueeze(0).repeat(batch_size), jet_conditions=jet_conditions, mask=mask)
+            midpoint_x = x_t + (start_vel * (t_end - t_start) / 2)
+            midpoint_vel = self.forward(x_t=midpoint_x, t=t_start + (t_end - t_start) / 2, jet_conditions=jet_conditions, mask=mask)
+            x_next = x_t + (t_end - t_start) * midpoint_vel
             return x_next
         else:
             raise NotImplementedError(f"Method {method} not implemented")
