9aad2b9e06b2cc76dc2cd1444e069c8e05725227
Arjun Sharma <arjun.sharma07@outlook.com>
2025-08-18 15:20:24 -0500

    Clean up dist

--- src/util/distributions.py
+++ src/util/distributions.py
@@ -3,24 +3,10 @@ import numpy as np
 
 def gen_initial_distribution(x_1 = None, current_batch_size = None, num_particles=None, num_particle_features=None, std=1, clamp_stddevs=None):
 
-    # if x_1 is not None:
-    #     dist = torch.randn_like(x_1) * std
-    # elif current_batch_size is not None and num_particles is not None and num_particle_features is not None:
-    #     dist = torch.randn((current_batch_size, num_particles, num_particle_features)) * std
-    # else:
-    #     raise ValueError("Either x_1 or current_batch_size, num_particles, and num_particle_features must be provided.")
-
-    # if clamp_stddevs is not None:
-    #     dist = torch.clamp(dist, -clamp_stddevs * std, clamp_stddevs * std)
-    
-    # # First column is energy, generate physical particles
-    # dist[:, :, 0] = torch.abs(dist[:, :, 0])  # Ensure energy is non-negative
-    # return dist
-
     if x_1 is not None:
         current_batch_size, num_particles = x_1.shape[:2]
 
-    E_c = torch.exp(torch.randn(current_batch_size, num_particles))  # Sample energy from appropriate distribution
+    E_c = torch.exp(torch.randn(current_batch_size, num_particles)) 
     theta = torch.arccos(1 - 2*torch.rand(current_batch_size, num_particles))
     phi = 2*torch.pi*torch.rand(current_batch_size, num_particles)
     p_x = E_c * torch.sin(theta) * torch.cos(phi)
