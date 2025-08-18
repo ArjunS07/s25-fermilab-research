0ea4ef493842bc2d4ec5a1b408d52e8612a714e7
Arjun Sharma <arjun.sharma07@outlook.com>
2025-08-17 20:17:19 -0500

    Try new clouds

--- src/util/distributions.py
+++ src/util/distributions.py
@@ -3,26 +3,28 @@ import numpy as np
 
 def gen_initial_distribution(x_1 = None, current_batch_size = None, num_particles=None, num_particle_features=None, std=1, clamp_stddevs=None):
 
-    if x_1 is not None:
-        dist = torch.randn_like(x_1) * std
-    elif current_batch_size is not None and num_particles is not None and num_particle_features is not None:
-        dist = torch.randn((current_batch_size, num_particles, num_particle_features)) * std
-    else:
-        raise ValueError("Either x_1 or current_batch_size, num_particles, and num_particle_features must be provided.")
+    # if x_1 is not None:
+    #     dist = torch.randn_like(x_1) * std
+    # elif current_batch_size is not None and num_particles is not None and num_particle_features is not None:
+    #     dist = torch.randn((current_batch_size, num_particles, num_particle_features)) * std
+    # else:
+    #     raise ValueError("Either x_1 or current_batch_size, num_particles, and num_particle_features must be provided.")
 
-    if clamp_stddevs is not None:
-        dist = torch.clamp(dist, -clamp_stddevs * std, clamp_stddevs * std)
+    # if clamp_stddevs is not None:
+    #     dist = torch.clamp(dist, -clamp_stddevs * std, clamp_stddevs * std)
     
-    # First column is energy, generate physical particles
-    dist[:, :, 0] = torch.abs(dist[:, :, 0])  # Ensure energy is non-negative
-    return dist
+    # # First column is energy, generate physical particles
+    # dist[:, :, 0] = torch.abs(dist[:, :, 0])  # Ensure energy is non-negative
+    # return dist
 
-def sample_massless_4momentum_clouds(n_clouds, cloud_size):
-    E_c = torch.exp(torch.randn(n_clouds, cloud_size))  # Sample energy from appropriate distribution
-    theta = np.arccos(1 - 2*torch.rand(n_clouds, cloud_size))
-    phi = 2*np.pi*torch.rand(n_clouds, cloud_size)
-    p_x = E_c * np.sin(theta) * np.cos(phi)
-    p_y = E_c * np.sin(theta) * np.sin(phi)
-    p_z = E_c * np.cos(theta)
+    if x_1 is not None:
+        current_batch_size, num_particles = x_1.shape[:2]
+
+    E_c = torch.exp(torch.randn(current_batch_size, num_particles))  # Sample energy from appropriate distribution
+    theta = torch.arccos(1 - 2*torch.rand(current_batch_size, num_particles))
+    phi = 2*torch.pi*torch.rand(current_batch_size, num_particles)
+    p_x = E_c * torch.sin(theta) * torch.cos(phi)
+    p_y = E_c * torch.sin(theta) * torch.sin(phi)
+    p_z = E_c * torch.cos(theta)
     p = torch.stack([E_c, p_x, p_y, p_z], dim=2)
     return p
