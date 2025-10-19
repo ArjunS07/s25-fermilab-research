bd36ee00e36f2e769271b79f54132b8eacaf6dbd
Arjun Sharma <arjun.sharma07@outlook.com>
2025-10-18 23:37:19 -0700

    Fix args mismatch

--- src/local.sh
+++ src/local.sh
@@ -3,6 +3,6 @@ UNIQUE_RUN_ID="$(date +%F_%H-%M-%S)--$POD_UID"
 OUTPUT_PATH="local_out/$UNIQUE_RUN_ID"
 mkdir $OUTPUT_PATH
 
-python3 data.py --num_particles=150 --output_path=${OUTPUT_PATH}
+python3 data.py --num_particles=30 --output_path=${OUTPUT_PATH} --jet_types g
 python3 jet_attr_model.py --output_path=${OUTPUT_PATH} --batch_size=1024 --num_epochs=10
-python3 train.py --batch_size=20 --num_epochs=5 --n_samples=5000 --n_layers=2 --n_train_samples=50 --n_samples=100 --output_path=${OUTPUT_PATH} --n_viz_samples=100 --prior_dist='jet_ref_frame' --integration_steps=4 --num_particles=150
+python3 train.py --batch_size=20 --num_epochs=2 --n_samples=5000 --n_layers=2 --n_train_samples=50 --n_samples=100 --output_path=${OUTPUT_PATH} --n_viz_samples=100 --integration_steps=4 --num_particles=30 --jet_types g
